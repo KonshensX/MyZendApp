7ecdb7730cdb55ab350afaf6a87d31760cdfc77f
Mohammed BAZA <mohamedbaza16@gmail.com>
2017-03-04 09:12:43 +0000

    Post module created

---
+++ module/Post/src/Post/Controller/PostController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Post\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class PostController extends AbstractActionController {
+
+    public function indexAction()
+    {
+        //TODO
+    }
+
+    public function addAction () {
+        //TODO
+    }
+
+    public function editAction () {
+        //TODO
+    }
+
+    public function deleteAction () {
+        //TODO
+    }
+
+}
